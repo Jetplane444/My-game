5a1cba7c6bff5663543eb45400adadfd607acc57
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-10-05 12:13:01 +0200

    Implement Enity base class and implemented collision.

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -5,18 +5,29 @@
 #include <Graphics/ResourceManager.hpp>
 #include <Math/Camera2D.hpp>
 
+#include <map>
+#include <string>
+#include <iostream>
+
+static std::map<Player::State, std::string> g_StateString = {
+    {Player::State::None, "None"},
+    {Player::State::Idle, "Idle"},
+    {Player::State::Running, "Running"}
+};
+
 using namespace Graphics;
 using namespace Math;
 
 Player::Player() = default;
 
 Player::Player(const glm::vec2& pos)
-    : transform{ pos }
-    , aabb{ {18, 10, 0}, {36, 43, 0} }
+    : Entity{ pos, AABB{{18, 10, 0}, {36, 43, 0}} }
 {
     auto idle_sprites = ResourceManager::loadSpriteSheet("assets/Warrior/SpriteSheet/Warrior_SheetnoEffect.png", 64, 44, 0, 0, BlendMode::AlphaBlend);
     IdleAnim = SpriteAnim(idle_sprites, 12, { {0, 1, 2, 3, 4, 5} });
     RunAnim = SpriteAnim(idle_sprites, 12, { {6, 7, 8, 9, 10, 11} });
+
+    transform.setAnchor({ 25,43 });
 }
 
 void Player::update(float deltaTime)
@@ -30,6 +41,16 @@ void Player::update(float deltaTime)
     velocity = (newPos - initialPos) / deltaTime;
 
     transform.setPosition(newPos);
+
+    // How to detect direction of player?
+    if (velocity.x < 0.0f)
+    {
+        transform.setScale({ -1, 1 });
+    }
+    else if (velocity.x > 0.0f)
+    {
+        transform.setScale({ 1, 1 });
+    }
 
     if (glm::length(velocity) > 0)
     {
@@ -58,28 +79,8 @@ void Player::draw(Graphics::Image& image, const Camera2D& camera)
 #if _DEBUG
     image.drawAABB(camera * getAABB(), Color::Yellow, {}, FillMode::WireFrame);
     auto pos = camera * transform;
-    image.drawText(Font::Default, "State...", pos[2][0], pos[2][1], Color::Black);
+    image.drawText(Font::Default, g_StateString[state], pos[2][0], pos[2][1], Color::Black);
 #endif
-}
-
-void Player::setPosition(const glm::vec2& pos)
-{
-    transform.setPosition(pos);
-}
-
-const glm::vec2& Player::getPosition() const
-{
-    return transform.getPosition();
-}
-
-void Player::translate(const glm::vec2& t)
-{
-    transform.translate(t);
-}
-
-const Math::AABB Player::getAABB() const
-{
-    return transform * aabb;
 }
 
 void Player::setState(State newState)
